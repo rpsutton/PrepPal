dd9ee72ed53c787ffd49eb6d64ef09c24c2103ae
rpsutton <57606697+rpsutton@users.noreply.github.com>
2025-02-01 12:54:14 -0500

    Initial Commit

---
+++ PrepPal/PrepPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepPalApp.swift
+//  PrepPal
+//
+//  Created by Paul Sutton on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PrepPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrepPalTests/PrepPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrepPalTests.swift
+//  PrepPalTests
+//
+//  Created by Paul Sutton on 2/1/25.
+//
+
+import Testing
+@testable import PrepPal
+
+struct PrepPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
